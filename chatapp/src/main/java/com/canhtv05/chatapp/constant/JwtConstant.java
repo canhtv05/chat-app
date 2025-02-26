3bd84b3a6ae70f0480f611ae1d941a7ba50a63a7
canhtv05 <canhtvph51036@gmail.com>
2025-02-27 01:33:22 +0700

    thêm client setup

--- chatapp/src/main/java/com/canhtv05/chatapp/constant/JwtConstant.java
+++
@@ -1,9 +0,0 @@
-package com.canhtv05.chatapp.constant;
-
-public class JwtConstant {
-
-    private JwtConstant() {}
-
-    public static final String JWT_HEADER = "Authorization";
-    public static final String SECRET_KEY = "iQ0RUaL+zsj6fH58UBk83qfnXel/bJIpoUZ/8Ybz33T2I/JVAxLfHC2DUrX6olWx";
-}
